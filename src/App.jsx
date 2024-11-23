0b9009b9f91e1fc225d2d399064514b8f76dfc5e
olgaluisa <luiisaolga11@gmail.com>
2024-11-22 21:04:35 -0300

    chore: import project structure into App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,24 @@
+import Header from '../src/Pages/Header';
+import About from './Pages/About';
+import ContactUs from './Pages/ContactUs';
+import Footer from './Pages/Footer';
+import Home from './Pages/Home';
+import Projects from './Pages/Projects';
+import Services from './Pages/Services';
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <main>
+        <Home />
+        <Services />
+        <About />
+        <Projects />
+        <ContactUs />
+      </main>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
