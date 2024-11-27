b9161d309f1da3890e1da6de9cfe10709fe14bad
vazvdr <azevedo.vanderson@hotmail.com>
2024-11-27 20:30:59 -0300

    Commit para fazer merge sem conflito

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,59 +1,89 @@
-import Image01 from '../assets/desktop/Image01.png';
-import Image02 from '../assets/desktop/Image02.png';
-import Image03 from '../assets/desktop/Image03.png';
+import DesktopImage01 from '../assets/desktop/Image01.png';
+import DesktopImage02 from '../assets/desktop/Image02.png';
+import DesktopImage03 from '../assets/desktop/Image03.png';
+import MobileImage01 from '../assets/mobile/images/image_1.png';
+import MobileImage02 from '../assets/mobile/images/image_2.png';
+import MobileImage03 from '../assets/mobile/images/image_3.png';
 
 const About = () => {
   return (
-    <section 
-      id="about" 
-      className="mt-20 px-[8%] hidden md:flex md:justify-right md:items-center"
-    >
-      <div className="md:w-[20%] md:flex md:flex-col md:gap-3 md:mr-[20px]">
-        <img 
-          className="md:w-[274px] md:h-[375px] object-cover rounded-[10px]" 
-          src={Image02} 
-          alt="Imagem 2" 
-        />
-        <img 
-          className="md:w-[274px] md:h-[375px] object-cover rounded-[10px]" 
-          src={Image03} 
-          alt="Imagem 3" 
-        />
-      </div>
-      
-      <div className="md:w-[20%] md:flex md:flex-col md:mr-[25px]">
-        <img 
-          className="md:w-[274px] md:h-[476px] object-cover rounded-[10px]" 
-          src={Image01} 
-          alt="Imagem 1" 
-        />
-      </div>
-
-      <div className="md:w-[45%] md:text-left">
-        <h2 className="font-bold text-4xl text-secondary-01 mb-5">
-          About Us
-        </h2>
-        <p className="font-normal text-lg leading-8 text-secondary-01 mb-5">
-          At 01-bit, we are passionate about turning ideas into unique digital
-          solutions. Our team of experts works closely with you to design,
-          develop, and implement tailored solutions that make a difference. With
-          an innovative and creative approach, we are involved in every step —
-          from user experience design to front-end and back-end development, all
-          the way to infrastructure.
-        </p>
-        <p className="font-normal text-lg leading-8 text-secondary-01">
-          We are committed to bringing your vision to the next level, merging
-          cutting-edge technology with creativity to create an impactful digital
-          future.
-        </p>
-
-        <div className="md:flex md:gap-4 mt-6">
-          <a href="#" className="text-purple-500 hover:text-purple-700">LinkedIn</a>
-          <a href="#" className="text-purple-500 hover:text-purple-700">Email</a>
-          <a href="#" className="text-purple-500 hover:text-purple-700">Instagram</a>
+    <>
+      <section 
+        id="about-desktop" 
+        className="hidden md:flex mt-20 px-[8%] justify-right items-center"
+      >
+        <div className="md:w-[20%] md:flex md:flex-col md:gap-3 md:mr-[20px]">
+          <img 
+            className="md:w-[274px] md:h-[375px] object-cover rounded-[10px]" 
+            src={DesktopImage02} 
+            alt="Imagem 2" 
+          />
+          <img 
+            className="md:w-[274px] md:h-[375px] object-cover rounded-[10px]" 
+            src={DesktopImage03} 
+            alt="Imagem 3" 
+          />
         </div>
-      </div>
-    </section>
+        <div className="md:w-[20%] md:flex md:flex-col md:mr-[25px]">
+          <img 
+            className="md:w-[274px] md:h-[476px] object-cover rounded-[10px]" 
+            src={DesktopImage01} 
+            alt="Imagem 1" 
+          />
+        </div>
+        <div className="md:w-[45%] md:text-left">
+          <h2 className="font-bold text-4xl text-secondary-01 mb-5">
+            About Us
+          </h2>
+          <p className="font-normal text-lg leading-8 text-secondary-01 mb-5">
+            At 01-bit, we are passionate about turning ideas into unique digital
+            solutions. Our team of experts works closely with you to design,
+            develop, and implement tailored solutions that make a difference. With
+            an innovative and creative approach, we are involved in every step — 
+            from user experience design to front-end and back-end development, all
+            the way to infrastructure.
+          </p>
+          <p className="font-normal text-lg leading-8 text-secondary-01">
+            We are committed to bringing your vision to the next level, merging 
+            cutting-edge technology with creativity to create an impactful digital 
+            future.
+          </p>
+          <div className="md:flex md:gap-4 mt-6">
+            <a href="#" className="text-purple-500 hover:text-purple-700">LinkedIn</a>
+            <a href="#" className="text-purple-500 hover:text-purple-700">Email</a>
+            <a href="#" className="text-purple-500 hover:text-purple-700">Instagram</a>
+          </div>
+        </div>
+      </section>
+      
+      <section 
+        id="about" 
+        className="md:hidden flex flex-col justify-end mt-20 px-10 max-w-[540px] mx-auto"
+      >
+        <div className="flex flex-row-reverse items-start gap-5">
+          <div>
+            <img className="mb-5" src={MobileImage01} alt="Perfil image" />
+            <img src={MobileImage03} alt="Perfil image" />
+          </div>
+          <div className="self-center">
+            <img src={MobileImage02} alt="Perfil image" />
+          </div>
+        </div>
+        <div>
+          <h2 className="text-left font-bold text-[28px] text-secondary-01 mb-5">
+            About Us
+          </h2>
+          <p className="text-left font-normal text-[13px] leading-6 text-secondary-01">
+            At 01-bit, we are passionate about turning ideas into unique digital 
+            solutions. Our team of experts works closely with you to design, 
+            develop, and implement tailored solutions that make a difference. With 
+            an innovative and creative approach, we are involved in every step — 
+            from user experience design to front-end and back-end development, all 
+            the way to infrastructure.
+          </p>
+        </div>
+      </section>
+    </>
   );
 };
 
--- src/Pages/Header.jsx
+++ src/Pages/Header.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
-import Logo from "../assets/desktop/logo.svg";
-import { ArrowUpCircle } from "react-feather";
+import DesktopLogo from "../assets/desktop/logo.svg";
+import MobileLogo from "../assets/mobile/logo/logo.svg";
+import { ArrowUpCircle, Menu } from "react-feather";
 
 const Header = () => {
   const [showScrollTopButton, setShowScrollTopButton] = useState(false);
@@ -21,14 +22,14 @@ const Header = () => {
 
   return (
     <>
-      <header className="mt-5 mx-10 flex justify-between items-center md:flex row hidden">
-        <img src={Logo} alt="Logo 01-bit" className="w-20" />
+      <header className="hidden md:flex mt-5 mx-10 justify-between items-center">
+        <img src={DesktopLogo} alt="Logo 01-bit" className="w-20" />
 
         <nav className="flex justify-center flex-grow">
           <ul className="flex gap-8">
             <li>
               <a
-                href="#home"
+                href="#home-desktop"
                 className="text-lg text-secondary-01 font-medium hover:text-primary-02 cursor-pointer"
               >
                 Home
@@ -36,7 +37,7 @@ const Header = () => {
             </li>
             <li>
               <a
-                href="#about"
+                href="#about-desktop"
                 className="text-lg text-secondary-01 font-medium hover:text-primary-02 cursor-pointer"
               >
                 About
@@ -44,7 +45,7 @@ const Header = () => {
             </li>
             <li>
               <a
-                href="#services"
+                href="#services-desktop"
                 className="text-lg text-secondary-01 font-medium hover:text-primary-02 cursor-pointer"
               >
                 Services
@@ -52,7 +53,7 @@ const Header = () => {
             </li>
             <li>
               <a
-                href="#projects"
+                href="#projects-desktop"
                 className="text-lg text-secondary-01 font-medium hover:text-primary-02 cursor-pointer"
               >
                 Projects
@@ -62,13 +63,40 @@ const Header = () => {
         </nav>
 
         <a
-          href="#contact"
+          href="#contact-desktop"
           className="flex items-center justify-center gap-2 bg-primary-01 py-4 px-7 text-otherColors-01 text-md rounded-[10px] hover:bg-primary-03 font-medium"
         >
           Contact Us
         </a>
       </header>
 
+      <header className="md:hidden mt-5 mx-5 flex justify-between items-center relative">
+        <img src={MobileLogo} alt="Logo 01-bit" />
+        <input type="checkbox" className="hidden" name="check" id="menu" />
+        <label htmlFor="menu">
+          <Menu className="text-secondary-01 cursor-pointer" size={40} />
+        </label>
+        <ul className="flex-col items-center justify-center gap-6 absolute top-[100%] right-1 w-[80%] h-[353px] bg-otherColors-01 rounded-lg shadow-boxShadow hidden z-50">
+        <li className="text-xl text-secondary-01 font-medium hover:text-primary-02">
+        <a href="#home">Home</a>
+      </li>
+      <li className="text-xl text-secondary-01 font-medium hover:text-primary-02">
+        <a href="#about">About</a>
+      </li>
+      <li className="text-xl text-secondary-01 font-medium hover:text-primary-02">
+        <a href="#services">Services</a>
+      </li>
+      <li className="text-xl text-secondary-01 font-medium hover:text-primary-02">
+        <a href="#projects">Projects</a>
+      </li>
+      <li className="text-xl text-secondary-01 font-medium hover:text-primary-02">
+        <a href="#contact">Contact Us</a>
+      </li>
+
+        </ul>
+      </header>
+
+      {/* Botão de Voltar ao Topo */}
       {showScrollTopButton && (
         <button
           onClick={scrollToTop}
